9c914280fd4b81919d2e9bf82190ed8dd5f9d404
Vasko <b.vasko@abv.bg>
2020-10-08 17:31:58 +0300

    first commit

---
+++ Models/Data.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Power_Monitoring.Models
+{
+    public class Data
+    {
+        public string MyName = "Vasko";
+
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Power_Monitoring.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
